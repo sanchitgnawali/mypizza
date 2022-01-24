20fb654c46632de804ae304ec70d4f1ceefc8465
Sanchit Gnawali <sanchitgnawali@gmail.com>
2022-01-23 21:28:41 -0600

    added animation to home and header component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,16 +4,16 @@ import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <div className="home container">
-      <motion.h2 animate={{ fontSize: 50, color: "#ff2994", x: 100, y: -100 }}>
-        Welcome to Pizza Joint
-      </motion.h2>
+    <motion.div
+      className="home container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
+      <motion.h2>Welcome to Pizza Joint</motion.h2>
       <Link to="/base">
-        <motion.button animate={{ scale: 1.5 }}>
-          Create Your Pizza
-        </motion.button>
+        <motion.button>Create Your Pizza</motion.button>
       </Link>
-    </div>
+    </motion.div>
   );
 };
 
