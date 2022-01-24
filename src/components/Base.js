06cfa9aecaac2fdbab03f9f60f897c98d6bc9730
Sanchit Gnawali <sanchitgnawali@gmail.com>
2022-01-23 21:43:28 -0600

    added transition to the Base, Header and Home components

--- src/components/Base.js
+++ src/components/Base.js
@@ -1,34 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Base = ({ addBase, pizza }) => {
-  const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
+  const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
 
   return (
-    <div className="base container">
-
+    <motion.div
+      className="base container"
+      initial={{ x: "100vw" }}
+      animate={{ x: 0 }}
+      transition={{ delay: 0.5, duration: 0.4, type: "spring", stiffness: 100 }}
+    >
       <h3>Step 1: Choose Your Base</h3>
       <ul>
-        {bases.map(base => {
-          let spanClass = pizza.base === base ? 'active' : '';
+        {bases.map((base) => {
+          let spanClass = pizza.base === base ? "active" : "";
           return (
             <li key={base} onClick={() => addBase(base)}>
-              <span className={spanClass}>{ base }</span>
+              <span className={spanClass}>{base}</span>
             </li>
-          )
+          );
         })}
       </ul>
 
       {pizza.base && (
-        <div className="next">
+        <motion.div
+          className="next"
+          initial={{ x: "-100vw" }}
+          animate={{ x: 0 }}
+        >
           <Link to="/toppings">
             <button>Next</button>
           </Link>
-        </div>
+        </motion.div>
       )}
-
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Base;
